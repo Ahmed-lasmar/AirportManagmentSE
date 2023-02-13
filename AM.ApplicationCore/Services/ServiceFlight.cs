9e5b58b78e4fce9fbad7c7476a86aa31692d1df5
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2023-02-13 11:49:14 +0100

    update

--- AM.ApplicationCore/Services/ServiceFlight.cs
+++ AM.ApplicationCore/Services/ServiceFlight.cs
@@ -9,6 +9,87 @@ namespace AM.ApplicationCore.Services
 {
     internal class ServiceFlight
     {
-        public List<Flight> Flights { get; set; };
+        public List<Flight> Flights { get; set; } = new List<Flight>();
+
+
+        //public List<DateTime> GetFlightDates(String destination)
+        //{
+        //    List<DateTime> result = new List<DateTime>();
+        //    for (int i = 0; i < Flights.Count; i++)
+        //    {
+        //        if (Flights[i].Destination.Equals(destination))
+        //        {
+        //            result.Add(Flights[i].FlightDate);
+        //        }
+        //    }
+        //    return result;
+        //}
+
+        /// methode foreach
+        public List<DateTime> GetFlightDates(String destination)
+        {
+            List<DateTime> result = new List<DateTime>();
+            foreach (var flight in Flights)
+            {
+                if (flight.Destination.Equals(destination))
+                {
+                    result.Add(flight.FlightDate);
+                }
+            }
+            return result;
+        }
+
+
+
+        public void ShowFlightDetails(Plane plane)
+        {
+            foreach (var flight in Flights)
+            {
+                if (flight.Plane == plane)
+                {
+
+                    Console.WriteLine(" Destination --> " + flight.Destination + " dans le date ---> " + flight.FlightDate);
+                }
+            }
+        }
+
+        public int ProgrammedFlighNumber(DateTime startDate)
+        {
+            int result = 0;
+            foreach (var flight in Flights)
+            {
+                if (flight.FlightDate >= startDate && flight.FlightDate <= startDate.AddDays(7))
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+        public float DurationAverage(string destination)
+        {
+            float result = 0;
+            int i = 0;
+            foreach (var flight in Flights)
+            {
+                if (flight.Destination.Equals(destination))
+                {
+                    result = (result + flight.);
+                    i++;
+                }
+            }
+            return result / i;
+        }
+
+        public List<Flight> OrderedDurationFlights()
+        {
+            List<Flight> flights = new List<Flight>();
+
+            foreach (var flight in Flights)
+            {
+                //flights.Add(flight.EstimatedDuration);
+            }
+            return flights;
+        }
     }
 }
