d29e267ec4101c5c9f07ed22e80a2fd33f0beaae
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2023-02-27 16:39:35 +0100

    TD4 I

--- AM.ApplicationCore/Domain/Flight.cs
+++ AM.ApplicationCore/Domain/Flight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace AM.ApplicationCore.Domain
     public class Flight
     {
         public int id { get; set; }
+        [ForeignKey("Plane")]
+        public int PlaneFK { get; set; }
         public string Destination { get; set; }
         public string Departure { get; set; }
         public DateTime FlightDate { get; set; }
--- AM.ApplicationCore/Domain/Passenger.cs
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,24 @@ namespace AM.ApplicationCore.Domain
 {
     public class Passenger
     {
-        public int Id { get; set; }
+        [Key]
+        [StringLength(7)]
+        public string PassportNumber { get; set; }
+
+        [Display(Name ="Date of Birth")]
+        [DataType(DataType.DateTime)]
         public DateTime BirthDate { get; set; }
-        public int PassportNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        //[EmailAddress]
         public string EmailAddress { get; set;}
+
+        [MinLength(3,ErrorMessage = "longueur minimale 3 caractères")]
+        [MaxLength(25,ErrorMessage = "longueur maximale 25 caractères")]
         public string FirstName { get; set;}
         public string LastName { get; set; }
+
+        [RegularExpression(@"^[0-9]{8}$")]
         public string TelNumber { get; set; }
         public ICollection<Flight> Flights { get; set; }
         //public bool CheckProfile(string first_name,string last_name)
